15a1ab593421e43ecb178c2163b68b01342ef1d5
Abboskhoja <akbarkhojayev@gmail.com>
2026-07-28 11:31:21 +0500

    fix

--- tic tak toe.py
+++ tic tak toe.py
@@ -1,45 +1,57 @@
-jadval = """ ____ ____ ____
+jadval = """
+-------------
 | 1 | 2 | 3 |
- ---- ---- ----
-| 4 | 5 | 6 | 
- ---- ---- ----
+-------------
+| 4 | 5 | 6 |
+-------------
 | 7 | 8 | 9 |
- ---- ---- ----
+-------------
 """
 
-win = ["123","456","789","159","147","357","369","258"]
+wins = [
+    "123", "456", "789",
+    "147", "258", "369",
+    "159", "357"
+]
+
 x_inputs = ""
 o_inputs = ""
-
-qoida = "123456789"
-
+numbers = "123456789"
 winner = False
 
-for i in range(1,10):
+for turn in range(1, 10):
+    player = "X" if turn % 2 else "O"
+    symbol = "❌" if player == "X" else "⭕"
+    inputs = x_inputs if player == "X" else o_inputs
+
+    choice = input(jadval + f"{player} o'yinchi raqam tanlasin: ")
+
+    while (
+        choice not in numbers
+        or choice in x_inputs
+        or choice in o_inputs
+    ):
+        choice = input(f"{player} o'yinchi qayta raqam tanlasin: ")
+
+    jadval = jadval.replace(choice, symbol)
+
+    if player == "X":
+        x_inputs += choice
+        inputs = x_inputs
+    else:
+        o_inputs += choice
+        inputs = o_inputs
+
+    for win in wins:
+        if all(i in inputs for i in win):
+            print(jadval)
+            print(f"{player} o'yinchi g'alaba qozondi!")
+            winner = True
+            break
+
     if winner:
         break
 
-    if i % 2 != 0:
-        x_input = input(jadval + "X o'yinchi raqam tanlasin: ")
-        while x_input not in qoida or x_input in o_inputs or x_input in x_inputs:
-            x_input = input("X o'yinchi qayta raqam tanlasin: ")
-
-        jadval = jadval.replace(x_input, "❌")
-        x_inputs += x_input
-        for x in win:
-            if x[0] in x_inputs and x[1] in x_inputs and x[2] in x_inputs:
-                print(jadval + "X o'yinchi galaba qozondi !!!")
-                winner = True
-    else:
-        o_input = input(jadval + "0 o'yinchi raqam tanlasin: ")
-        while o_input not in qoida or o_input in x_inputs or o_input in o_inputs:
-            o_input = input("0 o'yinchi qayta raqam tanlasin: ")
-
-        jadval = jadval.replace(o_input, "⭕")
-        o_inputs += o_input
-        for o in win:
-            if o[0] in o_inputs and o[1] in o_inputs and o[2] in o_inputs:
-                print(jadval + "0 o'yinchi galaba qozondi !!!")
-                winner = True
 if not winner:
-    print(jadval + "Durrang")
+    print(jadval)
+    print("Durrang!")
